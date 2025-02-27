3cbd908fe82ad5e2d57ff66bcad0e9e20f9e1ac3
arjunkmrm <arjunkmrm@gmail.com>
2025-02-27 15:14:48 +0530

    update smithery binary

--- Formula/smithery.rb
+++ Formula/smithery.rb
@@ -1,13 +1,11 @@
 class Smithery < Formula
   desc "Tool for installing and running containerized MCP servers"
-  homepage "https://github.com/smithery-ai/runner"
-  version "0.1.5"
-
-  depends_on "podman"
+  homepage "https://github.com/smithery-ai/cli"
+  version "0.1.1"
 
   if OS.mac?
-    url "https://github.com/smithery-ai/runner/releases/download/cli-beta.v0.1.5/smithery-darwin"
-    sha256 "89fc34c07d0e0f07e3f137dc12fd01fe585e123088432a15b9ed1fc910f0d9fe"
+    url "https://github.com/smithery-ai/cli/releases/download/cli-beta.v0.1.1/smithery-darwin"
+    sha256 "89e8b5ec69a8921355bc0d9c8abb6e48521ab18d8f93e7e77f4985161c115cde"
   else
     odie "Smithery is currently only supported on macOS"
   end
